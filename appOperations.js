d7005fc7b5ce8541a93a5c37413e406d4efc0996
Deus Paul <paulfurlan@circuitzero.com>
2024-03-01 00:40:53 -0800

    added new function

--- appOperations.js
+++ appOperations.js
@@ -1,8 +1,13 @@
 function multiply(num1, num2) {
-    return num1*num2;
+    return num1+num2;
+}
+
+function addition(num1, num2) {
+    return num1+num2;
 }
 
 module.exports = multiply;
 //module.exports = {
-//    multiply
+//    multiply,
+//    addition
 //};
