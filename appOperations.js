cee3c81b4d502e3601b9661d47a2f3ff5b2aee8c
Deus Paul <paulfurlan@circuitzero.com>
2024-03-01 13:27:37 -0800

    reverted

--- appOperations.js
+++ appOperations.js
@@ -2,12 +2,12 @@ function multiply(num1, num2) {
     return num1*num2;
 }
 
-function addition(num1, num2) {
+/*function addition(num1, num2) {
     return num1+num2;
-}
+}*/
 
-//module.exports = multiply;
-module.exports = {
+module.exports = multiply;
+/*module.exports = {
     multiply,
     addition
-};
+};*/
--- appOperations.test.js
+++ appOperations.test.js
@@ -1,10 +1,10 @@
 const appOperations = require('./appOperations');
 
 test('multiplication property of zero', () =>{
+    expect(appOperations(5,0)).toBe(0);
     //expect(appOperations.multiply(5,0)).toBe(0);
-    expect(appOperations.multiply(5,0)).toBe(0);
 });
 
-test('addition works properly', ()=>{
+/*test('addition works properly', ()=>{
     expect(appOperations.addition(5,6)).toBe(11);
-});
+});*/
