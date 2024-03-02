4af1e724e63077b2b703ac8fdfbe0f996ebc337c
Deus Paul <paulfurlan@circuitzero.com>
2024-03-02 02:22:51 -0800

    fixed multiply test case

--- appOperations.test.js
+++ appOperations.test.js
@@ -1,8 +1,8 @@
 const appOperations = require('./appOperations');
 
 test('multiplication property of zero', () =>{
-    expect(appOperations(5,0)).toBe(0);
-    //expect(appOperations.multiply(5,0)).toBe(0);
+    //expect(appOperations(5,0)).toBe(0);
+    expect(appOperations.multiply(5,0)).toBe(0);
 });
 
 test('addition works properly', ()=>{
