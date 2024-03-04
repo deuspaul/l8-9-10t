1f2cbe6ca0487d6a1bb9cc562becc36584e20f1a
Deus Paul <paulfurlan@circuitzero.com>
2024-03-03 23:23:50 -0800

    added subtract feature

--- appOperations.js
+++ appOperations.js
@@ -1,11 +1,13 @@
-service_token = "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"
-
 function multiply(num1, num2) {
     return num1*num2;
 }
 
 function addition(num1, num2) {
     return num1+num2;
+}
+
+function subtract(num1, num2) {
+    return num1-num2;
 }
 
 //module.exports = multiply;
